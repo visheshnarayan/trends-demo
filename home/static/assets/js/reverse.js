902b994e52133d26aa7b44e37271222c9c06c7a3
visheshnarayan <visheshnarayan@icloud.com>
2023-04-25 19:14:15 -0400

    fixed slides not loading and added comments

--- home/static/assets/js/reverse.js
+++ home/static/assets/js/reverse.js
@@ -1,11 +1,19 @@
+/**
+ * TODO: DELETE ALL console.log after done debug
+ * issue: all texts not loaded into div
+ * 
+ * notes: 
+ * scope issue? are the slides not seeing the text? 
+ * 
+ * TASK: show all slides
+ * NEXT: unpack new datatype 
+ */
 function update_carousel(){
     console.log("inside carousel function");
     console.warn(reverse_context);
 
-    //reverse_context -> data 
-    // TODO: write jquery to update carousel 
-    // TODO: refactor with jquery
-    // TODO: style the slides so they look correct
+    //reverse_context["docs"] -> text data
+    // will be changed to access text inside new data structure
     const texts = reverse_context['docs'];
     texts.forEach(text => {
         /**
@@ -13,13 +21,8 @@ function update_carousel(){
          * --> this is done so the "data-active" meta-tag is assigned to the first text inserted and so the preview is not blank on load 
          * every other node created and joined the same way
          */
-        var list
-
-        // new
-        texts.indexOf(text) == 0 ? list = $("#first-slide") : list = $("<li></li>", {"class": "slides"}).appendTo("slides");
-        // list.addClass("slide");
-        // var $div = $("<div>", {id: "foo", "class": "a"});
-        // if (texts.indexOf(text) == 0) {list = $("#first-slide")} else {list = $("div", {"class": "slide"})}
+        var list;
+        texts.indexOf(text) == 0 ? list = $("#first-slide") : list = $("<li></li>", {"class": "slide"}).appendTo("#slides");
     
         /**
          * create element nodes
@@ -41,33 +44,36 @@ function update_carousel(){
      * left and right buttons
      */
     const buttons = document.querySelectorAll("[data-carousel-button]")
-
     buttons.forEach(button => {
         button.addEventListener("click", () => {
-            // go right if "next" button: go left if not
-            const offSet = button.dataset.carouselButton === "next" ? 1 : -1
-
-            // go into data-carousel and select data-slides
+            /**
+             * go right if "next" button: go left if not
+             * go into data-carousel and select data-slides
+             */
+            const offSet = button.dataset.carouselButton === "next" ? 1 : -1;
             const slides = button
                 .closest("[data-carousel]")
-                .querySelector("[data-slides]")
+                .querySelector("[data-slides]");
+            const activeSlide = slides.querySelector(["[data-active]"]);
 
-            const activeSlide = slides.querySelector(["[data-active]"])
+            /**
+             * index of active slide + offset
+             */
+            let newIndex = [...slides.children].indexOf(activeSlide) + offSet;
 
-            // index of active slide + offset
-            let newIndex = [...slides.children].indexOf(activeSlide) + offSet
+            /**
+             * if first send to last slide
+             * if index greater than length (or = to) send to first slide
+             */
+            if (newIndex < 0) newIndex = slides.children.length - 1;
+            if (newIndex >= slides.children.length) newIndex = 0;
 
-            // if first send to last slide
-            if (newIndex < 0) newIndex = slides.children.length - 1
-
-            // if index greater than length send to first slide
-            if (newIndex >= slides.children.length) newIndex = 0
-
-            // new active slide
-            slides.children[newIndex].dataset.active = true
-
-            // delete old active slide
-            delete activeSlide.dataset.active
+            /**
+             * create new active slide
+             * delete old one
+             */
+            slides.children[newIndex].dataset.active = true;
+            delete activeSlide.dataset.active;
         })
-    })
+    });
 };
